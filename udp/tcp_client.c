f1bfdd605eb20d427a508e1b76fbb8777ce3d5c3
BreezingLad <breezing_lad@qq.com>
2017-11-14 07:34:15 -0500

    aynchronous client to send big data

--- udp/tcp_client.c
+++ udp/tcp_client.c
@@ -15,6 +15,29 @@
 #include<arpa/inet.h>
 
 #include<unistd.h>
+#include<fcntl.h>
+#include<errno.h>
+//#include<math.h>
+
+#define max(a,b)    ((a) > (b)) ? (a) : (b)
+
+int set_nonblack(int fd)
+{
+    int flags;
+    if((flags = fcntl(fd, F_GETFL)) == -1)
+    {
+        return -1;
+    }
+
+    flags |= O_NONBLOCK;
+
+    if(fcntl(fd, F_SETFL, flags) == -1)
+    {
+        return -1;
+    }
+
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -54,25 +77,118 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-
-    int iTransport = write(sockfd, szMsg, iMsgSize);
-
-    printf("send %d \n", iTransport);
-
-    int nRead = 0;
-    iTransport = 0;
- 
     
-    do
+
+    set_nonblack(sockfd);
+
+    int     iput = fileno(stdin);
+    int     maxfd, stdineof = 0;
+    fd_set  rset;
+    FD_ZERO(&rset);
+    
+
+    int iTransport = 0;
+
+    int iRead = 0, iWrite = 0, i = 0;
+    while(1)
     {
-        nRead = read(sockfd, szMsg, iMsgSize);
-        iTransport += nRead;
-        printf("read: %d, total: %d, buffer size:%d\n", nRead, iTransport, iMsgSize);
-    }while(nRead > 0 && iTransport < iMsgSize);
-    
+        if(stdineof == 0)
+            FD_SET(iput, &rset);
+        FD_SET(sockfd, &rset);
 
-    //iTransport = read(sockfd, szMsg, iMsgSize);
-    printf("recieve %d \n", iTransport);
+        maxfd = max(iput, sockfd) + 1;
+        if(select(maxfd, &rset, NULL, NULL, NULL) < 0)
+        {
+            
+        }
+
+        if(FD_ISSET(sockfd, &rset))
+        {
+            i = read(sockfd, szMsg, iMsgSize);
+            if(i < 0)
+            {
+                continue;
+            }
+            iRead += i;
+
+            printf("read: %d, total: %d, buffer size:%d\n", i, iRead, iMsgSize);
+            if(iRead >= iMsgSize)
+            {
+                printf("recieve %d \n", iRead);
+                //break;
+            }
+            /*
+            int nRead = 0;
+            iTransport = 0;
+
+            do
+            {
+                nRead = read(sockfd, szMsg, iMsgSize);
+                if(nRead < 0)
+                {
+                    if(errno == EAGAIN)
+                    {
+                        continue;
+                    }
+                }
+                iTransport += nRead;
+                printf("read: %d, total: %d, buffer size:%d\n", nRead, iTransport, iMsgSize);
+            }while(iTransport < iMsgSize);
+            //iTransport = read(sockfd, szMsg, iMsgSize);
+            printf("recieve %d \n", iTransport);
+            break;
+            */
+        }
+
+        if(FD_ISSET(iput, &rset))
+        {
+            if(stdineof == 2)
+            {
+                continue;
+            }
+
+            i = write(sockfd, szMsg, iMsgSize - iWrite);
+            if(i < 0)
+            {
+                continue;
+            }
+            iWrite += i;
+
+            printf("write: %d, total: %d, buffer size: %d\n", i, iWrite, iMsgSize);
+
+            if(iWrite < iMsgSize)
+            {
+                continue;
+            }
+
+            /*
+            int nWrite = 0;
+            do
+            {
+                
+                nWrite = write(sockfd, szMsg, iMsgSize);
+                iTransport += nWrite;
+                printf("write: %d, total: %d, buffer size: %d\n", nWrite, iTransport, iMsgSize);
+
+
+            }while(iTransport < iMsgSize);
+
+
+            printf("send %d \n", iTransport);
+
+            */
+
+            printf("send %d \n", iWrite);
+            stdineof = 2;
+
+        }
+
+
+    }
+
+       
+
+
 
 
     return 0;
